754eb9c9f1c56e5340d3eed7990798b51a7d784b
roksilic <roksilic.net@gmail.com>
2026-08-07 18:02:48 +0200

    Readme update

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ Python FastAPI search
 ## Requirements
 - python3
 - uv
+- postgresql server to connect or use docker setup
+
+## Pre-requirements
+- Copy `.env_template` and save it as `.env`
+
 
 ## How to run
 ```bash
--- app/api/movie_api.py
+++ app/api/movie_api.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter
+from sqlalchemy import text
+from app.database import SessionDep
 
 router = APIRouter(prefix="/api/v1/movies", tags=["movies"])
 
 @router.get("/")
-async def index():
-    return {"Hello": "World"}
+async def index(db: SessionDep):
+    rows = db.exec(text("SELECT * FROM drugs LIMIT 1")).mappings().all()
+    return [dict(row) for row in rows]
 
 @router.get("/{movie_id}")
 async def get_movie(movie_id: int):
