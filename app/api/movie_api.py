5acee64c12068e558e1ac719005a1202a25171fc
roksilic <roksilic.net@gmail.com>
2026-08-08 16:22:59 +0200

    Api updates

--- app/api/movie_api.py
+++ app/api/movie_api.py
@@ -6,9 +6,13 @@ router = APIRouter(prefix="/api/v1/movies", tags=["movies"])
 
 @router.get("/")
 async def index(db: SessionDep):
-    rows = db.exec(text("SELECT * FROM drugs LIMIT 1")).mappings().all()
+    rows = db.exec(text("SELECT * FROM movies LIMIT 1")).mappings().all()
     return [dict(row) for row in rows]
 
 @router.get("/{movie_id}")
-async def get_movie(movie_id: int):
-    return {"movie_id": movie_id}
+async def get_movie(movie_id: int, db: SessionDep):
+    row = db.exec(text("SELECT * FROM movies WHERE id = :movie_id"), {"movie_id": movie_id}).mappings().first()
+    if row is None:
+        return {"error": "Movie not found"}
+    
+    return dict(row)
