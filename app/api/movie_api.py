3ebd8811246f985c364291d580f4856f53751be0
roksilic <roksilic.net@gmail.com>
2026-08-08 16:33:20 +0200

    Starting with api

--- app/api/movie_api.py
+++ app/api/movie_api.py
@@ -1,18 +1,20 @@
 from fastapi import APIRouter
 from sqlalchemy import text
 from app.database import SessionDep
+from app.models.movie import Movie
+from array import array
 
 router = APIRouter(prefix="/api/v1/movies", tags=["movies"])
 
 @router.get("/")
-async def index(db: SessionDep):
-    rows = db.exec(text("SELECT * FROM movies LIMIT 1")).mappings().all()
-    return [dict(row) for row in rows]
+def index(db: SessionDep):
+    movies = db.exec(text("SELECT * FROM movies LIMIT 100")).mappings().all()
+    return [dict(movie) for movie in movies]
 
 @router.get("/{movie_id}")
-async def get_movie(movie_id: int, db: SessionDep):
-    row = db.exec(text("SELECT * FROM movies WHERE id = :movie_id"), {"movie_id": movie_id}).mappings().first()
-    if row is None:
+def get_movie(movie_id: int, db: SessionDep) -> Movie:
+    movie = db.get(Movie, movie_id)
+    if movie is None:
         return {"error": "Movie not found"}
     
-    return dict(row)
+    return dict(movie)
